72e4a5a11e59a1d244a5f3346125c335187b610c
hbajwaa <bajwahassan3@outlook.com>
2020-07-06 16:12:20 -0400

    switched to fetch method

--- application.js
+++ application.js
@@ -1,24 +1,18 @@
-var elementAdditionPosition = "beforeend";
-getTasks()
-function getTasks() {
-  var xhttp = new XMLHttpRequest();
-  xhttp.onreadystatechange = function () {
-    if (this.readyState == 4 && this.status == 200) {
-      // Add the json stuff
-      let container = document.getElementById("container");
-      let responses = JSON.parse(this.responseText);
+const elementAdditionPosition = "beforeend";
 
-      for (var i = 0; i < responses.length; i++) {
-        container.insertAdjacentHTML(
-          elementAdditionPosition,
-          createResponse(responses[i])
-        );
-      }
+function getTasks() {
+  fetch("http://localhost:3000/todos")
+  .then(function(response) {
+    return response.json()
+    })
+  .then(function(json) {
+    let container = document.getElementById("container");
+    for (var i = 0; i < json.length; i++) {
+      container.insertAdjacentHTML(elementAdditionPosition,createResponse(json[i]));
     }
-  };
-  xhttp.open("GET", "http://localhost:3000/todos", true);
-  xhttp.send();
+  })
 }
+
 
 window.sendForm = function (event) {
   event.preventDefault();
@@ -27,7 +21,6 @@ window.sendForm = function (event) {
   xhttp.onload = function (event) {
     let container = document.getElementById("container");
     let response = JSON.parse(event.target.response);
-
     container.insertAdjacentHTML(
       elementAdditionPosition,
       createResponse(response)
@@ -41,13 +34,14 @@ window.sendForm = function (event) {
 function createResponse(response) {
   return (
     `<div class="task" id="${response.id}" onclick="deleteResponse('${response.id}')">` +
-    "[" +
-    response.id +
-    "]: " +
     response.task +
     "<br />" +
     "</div>"
   );
+}
+
+function edit(id) {
+  fetch
 }
 
 function deleteResponse(responseId) {
@@ -55,3 +49,4 @@ function deleteResponse(responseId) {
   const task = document.getElementById(responseId)
   task.parentNode.removeChild(task)
 }
+getTasks()
