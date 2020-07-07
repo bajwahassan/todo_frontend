bde82a7d0366dfc8bf2b2b52270b01e9955b4abb
hbajwaa <bajwahassan3@outlook.com>
2020-07-07 19:05:13 -0400

    refactored some code

--- application.js
+++ application.js
@@ -1,52 +1,24 @@
-const elementAdditionPosition = "beforeend";
+document.addEventListener("DOMContentLoaded", () => {
+  Api.getTasks()
+})
 
-function getTasks() {
-  fetch("http://localhost:3000/todos")
-  .then(function(response) {
-    return response.json()
-    })
-  .then(function(json) {
-    let container = document.getElementById("container");
-    for (var i = 0; i < json.length; i++) {
-      container.insertAdjacentHTML(elementAdditionPosition,createResponse(json[i]));
-    }
-  })
-}
+const myForm = document.getElementById("todo_form")
 
-
-window.sendForm = function (event) {
-  event.preventDefault();
+myForm.addEventListener("submit", (e) => {
+  e.preventDefault()
   var xhttp = new this.XMLHttpRequest();
   xhttp.open("POST", "http://localhost:3000/todos", true);
-  xhttp.onload = function (event) {
+
+  xhttp.onload = function (e) {
     let container = document.getElementById("container");
     let response = JSON.parse(event.target.response);
-    container.insertAdjacentHTML(
-      elementAdditionPosition,
-      createResponse(response)
-    );
+    container.insertAdjacentHTML("beforeend", Api.createResponse(response));
   };
   var formData = new this.FormData(document.getElementById("todo_form"));
   xhttp.send(formData);
   document.getElementById("task_input").value = "";
-};
-
-function createResponse(response) {
-  return (
-    `<div class="task" id="${response.id}" onclick="deleteResponse('${response.id}')">` +
-    response.task +
-    "<br />" +
-    "</div>"
-  );
-}
-
-function edit(id) {
-  fetch
-}
+})
 
 function deleteResponse(responseId) {
-  fetch( `http://localhost:3000/todos/${responseId}`, { method: "delete" } )
-  const task = document.getElementById(responseId)
-  task.parentNode.removeChild(task)
+  Api.deleteTask(responseId)
 }
-getTasks()
